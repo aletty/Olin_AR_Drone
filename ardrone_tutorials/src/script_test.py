b761b535a855a2594682611c701f471ea9fdd952
Silas Hughes <silas.hughes@students.olin.edu>
2013-10-17 12:42:54 -0400

    update script, no debounce

--- ardrone_tutorials/src/script_test.py
+++ ardrone_tutorials/src/script_test.py
@@ -19,19 +19,60 @@ from sensor_msgs.msg import Joy
 # Finally the GUI libraries
 from PySide import QtCore, QtGui
 
+# define the default mapping between joystick buttons and their corresponding actions
+ButtonEmergency = 8
+ButtonLand      = 1
+ButtonTakeoff   = 0
+ButtonFlattrim  = 7
+ButtonHover     = 2
+
+# define the default mapping between joystick axes and their corresponding directions
+AxisRoll        = 0
+AxisPitch       = 1
+AxisYaw         = 3
+AxisZ           = 4
+
+# define the default scaling to apply to the axis inputs. useful where an axis is inverted
+ScaleRoll       = 1.0
+ScalePitch      = 1.0
+ScaleYaw        = 1.0
+ScaleZ          = 1.0
+
+button_last_state = 0
 
 def RunScript():
-	rospy.loginfo("Starting Script")
-	rospy.Timer(5.0, controller.CallFlattrim(),True)
-	controller.Flattrim()
-	# rospy.sleep(5.)
-	rospy.loginfo("Takeing Off")
-	controller.SendTakeoff()
-	controller.SendHover()
-	rospy.Timer(5.0, controller.SendLand(), True)
-	# rospy.sleep(5.)
-	controller.SendLand()
-	rospy.loginfo("Landed")
+  rospy.loginfo("Starting Script")
+  controller.Flattrim()
+  rospy.sleep(5.)
+  rospy.loginfo("Takeing Off")
+  controller.SendTakeoff()
+  controller.SendHover()
+  rospy.sleep(5.)
+  controller.SendLand()
+  rospy.loginfo("Landed")
+
+
+# handles the reception of joystick packets
+def ReceiveJoystickMessage(data):
+	if data.buttons[ButtonEmergency]==1:
+		rospy.loginfo("Emergency Button Pressed")
+		controller.SendEmergency()
+	elif data.buttons[ButtonLand]==1:
+		rospy.loginfo("Land Button Pressed")
+		controller.SendLand()
+	elif data.buttons[ButtonTakeoff]==1:
+		rospy.loginfo("Takeoff Button Pressed")
+		controller.SendTakeoff()
+	elif data.buttons[ButtonFlattrim]==1:
+		rospy.loginfo("Flattrim Button Pressed")
+		controller.CallFlattrim()
+	elif data.buttons[ButtonHover]==1:
+		#rospy.loginfo("Hover Button Pressed")
+		#controller.SendHover()
+		rospy.loginfo("SCRIPT Button Pressed")
+		RunScript()
+	else:
+		controller.SetCommand(data.axes[AxisRoll]/ScaleRoll,data.axes[AxisPitch]/ScalePitch,data.axes[AxisYaw]/ScaleYaw,data.axes[AxisZ]/ScaleZ)
 
 # Setup the application
 if __name__=='__main__':
@@ -39,18 +80,32 @@ if __name__=='__main__':
 	# Firstly we setup a ros node, so that we can communicate with the other packages
 	rospy.init_node('script_controller')
 
+	# Next load in the parameters from the launch-file
+	ButtonEmergency = int (   rospy.get_param("~ButtonEmergency",ButtonEmergency) )
+	ButtonLand      = int (   rospy.get_param("~ButtonLand",ButtonLand) )
+	ButtonTakeoff   = int (   rospy.get_param("~ButtonTakeoff",ButtonTakeoff) )
+	ButtonFlattrim  = int (		rospy.get_param("~ButtonFlattrim", ButtonFlattrim) )
+	ButtonHover			= int (		rospy.get_param("~ButtonHover", ButtonHover) )
+	AxisRoll        = int (   rospy.get_param("~AxisRoll",AxisRoll) )
+	AxisPitch       = int (   rospy.get_param("~AxisPitch",AxisPitch) )
+	AxisYaw         = int (   rospy.get_param("~AxisYaw",AxisYaw) )
+	AxisZ           = int (   rospy.get_param("~AxisZ",AxisZ) )
+	ScaleRoll       = float ( rospy.get_param("~ScaleRoll",ScaleRoll) )
+	ScalePitch      = float ( rospy.get_param("~ScalePitch",ScalePitch) )
+	ScaleYaw        = float ( rospy.get_param("~ScaleYaw",ScaleYaw) )
+	ScaleZ          = float ( rospy.get_param("~ScaleZ",ScaleZ) )
+
 
 	# Now we construct our Qt Application and associated controllers and windows
 	app = QtGui.QApplication(sys.argv)
 	display = DroneVideoDisplay()
 	controller = BasicDroneController()
 
-	# print'running script'
-	# RunScript()
+	# subscribe to the /joy topic and handle messages of type Joy with the function ReceiveJoystickMessage
+	subJoystick = rospy.Subscriber('/joy', Joy, ReceiveJoystickMessage)
 	
 	# executes the QT application
 	display.show()
-	RunScript()
 	status = app.exec_()
 
 	# and only progresses to here once the application has been shutdown
